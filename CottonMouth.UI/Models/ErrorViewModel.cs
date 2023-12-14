eba618891e7f8693715cdd997134066cce8659f4
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2023-12-14 11:55:49 +0530

    Delete CottonMouth.UI directory

--- CottonMouth.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CottonMouth.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
